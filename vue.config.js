bb1b64ad65c3b0c68ddc3e02fa1a9c6692049492
dongyoungkim <kdy41000@naver.com>
2022-06-06 11:12:25 +0900

    git pages setting

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   pwa: {
-    name: 'VUESTAGRAM',  // 설정 할 앱이름(manifest, service-worker설정, 빌드할 때 마다 동일하게 빌드될 수 있도록 설정가능)
+    name: 'VUESTAGRAM',  // 설정 할 앱이름(manifest.js, service-worker.js 설정, 빌드할 때 마다 동일하게 빌드될 수 있도록 설정가능)
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     workboxOptions: {
       exclude: [/\.map$/, /manifest\.json$/, 'index.html']
     }
-  }
+  },
+  outputDir: './docs',
+  publicPath: '/VueStagram/'
 }
